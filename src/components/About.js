a9ebede1ea768de22a08269588034db471dc90cf
Vincent Rabatel <vincent.rabatel@gmail.com>
2023-08-19 20:41:32 +0200

    Add two different states to Collapse component and update styling

--- src/components/About.js
+++ src/components/About.js
@@ -6,23 +6,31 @@ import Collapse from "./Collapse"
 
 import "../styles/About.scss"
 
-// Setup banner data
+// Data to setup the banner
 import bannerImage from "../assets/banner-about.jpg"
 const bannerText = ""
 const bannerAlt = "Mountains and a river in the background"
+
+// Texts that will fill the Collapse components
+const fiabilityText = "Les annonces postées sur Kasa garantissent une fiabilité totale. Les photos sont conformes aux logements, et toutes les informations sont régulièrement vérifiées par nos équipes."
+const respectText = "La bienveillance fait partie des valeurs fondatrices de Kasa. Tout comportement discriminatoire ou de perturbation du voisinage entraînera une exclusion de notre plateforme."
+const serviceText = "La bienveillance fait partie des valeurs fondatrices de Kasa. Tout comportement discriminatoire ou de perturbation du voisinage entraînera une exclusion de notre plateforme."
+const securityText = "La sécurité est la priorité de Kasa. Aussi bien pour nos hôtes que pour les voyageurs, chaque logement correspond aux critères de sécurité établis par nos services. En laissant une note aussi bien à l'hôte qu'au locataire, cela permet à nos équipes de vérifier que les standards sont bien respectés. Nous organisons également des ateliers sur la sécurité domestique pour nos hôtes."
 
 export default function About() {
     return (
         <React.Fragment>
             <Header />
             <main>
+                {/* Banner */}
                 <Banner image={bannerImage} text={bannerText} alt={bannerAlt} />
 
+                {/* Collapses */}
                 <div className="collapse-container">
-                    <Collapse title="Fiabilité" />
-                    <Collapse title="Respect" />
-                    <Collapse title="Service" />
-                    <Collapse title="Sécurité" />
+                    <Collapse title="Fiabilité" text={fiabilityText} />
+                    <Collapse title="Respect"   text={respectText} />
+                    <Collapse title="Service"   text={serviceText} />
+                    <Collapse title="Sécurité"  text={securityText} />
                 </div>
             </main>
             <Footer />
--- src/components/Collapse.js
+++ src/components/Collapse.js
@@ -1,13 +1,36 @@
-import React from "react"
+import { useState } from "react";
 
-import "../styles/Collapse.scss"
-import arrow from "../assets/arrow.svg"
+import "../styles/Collapse.scss";
+import arrow from "../assets/arrow.svg";
 
-export default function Collapse({title}) {
+export default function Collapse({title, text}) {
+    const [collapseState, setCollapseState] = useState(false);
+
+    function UpdateCollapse() {
+        switch(collapseState) {
+            case false:
+                setCollapseState(true);
+                console.log("Was false, going to true")
+            break;
+    
+            case true:
+                setCollapseState(false); 
+                console.log("Was true, going to false")
+            break;
+    
+            default:
+                console.error("What the f")
+        }
+    }
+
     return (
         <div className="collapse">
-            <h2 className="collapse__title">{title}</h2>
-            <img className="collapse__arrow" src={arrow} alt =""></img>
+            <button className="collapse__btn" onClick={UpdateCollapse}>
+                <h2 className="collapse__btn__title">{title}</h2>
+                <img className={`collapse__btn__arrow ${collapseState ? "collapse__btn__arrow--open" : "collapse__btn__arrow--close"}`} src={arrow} alt =""></img>
+            </button>
+            <p className={`collapse__text ${collapseState ? "collapse__text--open" : "collapse__text--close"}`}>{text}</p>
         </div>
+
     )
 }
