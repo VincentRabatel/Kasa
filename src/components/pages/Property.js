c2bbfd6253042ec22cd6e287705480a71291a5a9
Vincent Rabatel <vincent.rabatel@gmail.com>
2023-08-22 17:00:40 +0200

    Add a basic stucture to properties' informations page

--- src/components/pages/Property.js
+++ src/components/pages/Property.js
@@ -2,23 +2,50 @@ import React from "react"
 import Header from "../Header"
 import Footer from "../Footer"
 import Slideshow from "../Slideshow"
+import Collapse from "../Collapse"
 
 import "./Property.scss"
 
 import propertiesData from "../../datas/properties.json"
 
 export default function Property() {
-
     const id = getPropertyIDFromUrl();
-    const property = propertiesData.find((p) => p.id === id); //console.log(property);
+    const property = propertiesData.find((p) => p.id === id);
 
     return (
         <React.Fragment>
             <Header />
                 <main className="property-container">
+
+                    {console.log("This will be printed twice")}
+
                     <Slideshow pictures={property.pictures} />
 
-                    <h1>{property.title}</h1>
+                    <div className="infos">
+                        {/* Left tab */}
+                        <div>
+                            <h2 className="infos__title">{property.title}</h2>
+                            <h3 className="infos__location">{property.location}</h3>
+                            <div className="infos__tags-container">
+                                <div>Tag 1</div>
+                                <div>Tag 2</div>
+                                <div>Tag 3</div>
+                            </div>
+                        </div>
+
+                        {/* Right tab */}
+                        <div>
+                            <div className="infos__host">
+                                <h3 className="infos__host__name">{property.host.name}</h3>
+                                <img className="infos__host__picture" src={property.host.picture} alt="Profile of the host"></img>
+                            </div>
+                        </div>
+                    </div>
+
+                    <div className="collapses-container">
+                        <Collapse title="Description" text={property.description}/>
+                        <Collapse title="Équipements" text={property.equipments}/>
+                    </div>
                 </main>
             <Footer />
         </React.Fragment>
