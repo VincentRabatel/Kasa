87ea5bb5dff9e115818ff89763d3338fe5406709
Vincent Rabatel <vincent.rabatel@gmail.com>
2023-08-22 14:20:21 +0200

    Get all property informations from the id in the URL

--- src/components/pages/Property.js
+++ src/components/pages/Property.js
@@ -4,12 +4,27 @@ import Footer from "../Footer"
 
 import "./Property.scss"
 
+import propertiesData from "../../datas/properties.json"
+
 export default function Property() {
+
+    const id = getPropertyIDFromUrl();
+    const property = propertiesData.find((p) => p.id === id); console.log(property)
+
     return (
         <React.Fragment>
             <Header />
-                <main>This is a housing page</main>
+                <main>
+                    <h1>{property.title}</h1>
+                </main>
             <Footer />
         </React.Fragment>
     )
-} 
+}
+
+function getPropertyIDFromUrl() {
+    const url = window.location.href;
+    const id = url.slice(url.length - 8, url.length);
+
+    return id;
+}
