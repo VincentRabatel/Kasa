84921546ef9129422b328105534d044cb47b4da8
Vincent Rabatel <vincent.rabatel@gmail.com>
2023-08-22 14:22:07 +0200

    Debug header's navbar paths

--- src/components/Header.js
+++ src/components/Header.js
@@ -10,10 +10,10 @@ export default function Header() {
             <nav className="header__nav-bar">
                 <ul className="header__nav-bar__list">
                     <li>
-                        <a className="header__nav-bar__element" href="./">Accueil</a>
+                        <a className="header__nav-bar__element" href="/">Accueil</a>
                     </li>
                     <li>
-                        <a className="header__nav-bar__element" href="./about">A propos</a>
+                        <a className="header__nav-bar__element" href="/about">A propos</a>
                     </li>
                 </ul>
             </nav>
