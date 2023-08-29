9d2189d6cb79bfbc106e1e87255507a7637d7a7a
Vincent Rabatel <vincent.rabatel@gmail.com>
2023-08-29 17:04:48 +0200

    Clean alt attributes of img markups

--- src/components/Collapse.js
+++ src/components/Collapse.js
@@ -25,7 +25,11 @@ export default function Collapse({title, content}) {
         <div className="collapse">
             <button className="collapse__btn" onClick={updateCollapse}>
                 <h2 className="collapse__btn__title">{title}</h2>
-                <img className={`collapse__btn__arrow ${collapseState ? "collapse__btn__arrow--open" : "collapse__btn__arrow--close"}`} src={arrow} alt =""></img>
+                <img 
+                    className={`collapse__btn__arrow ${collapseState ? "collapse__btn__arrow--open" : "collapse__btn__arrow--close"}`} 
+                    src={arrow} 
+                    alt={collapseState ? "Arrow to open the menu" : "Arrow to open the menu"}>
+                </img>
             </button>
             <div className={`collapse__content ${collapseState ? "collapse__content--open" : "collapse__content--close"}`}>{content}</div>
         </div>
--- src/components/PropertyCard.js
+++ src/components/PropertyCard.js
@@ -3,7 +3,7 @@ import "./PropertyCard.scss"
 export default function PropertyCard({id, title, picture}) {
     return (
         <a className="property-card" href={`../property/${id}`}>
-            <img className="property-card__img" src={picture} alt=""></img>
+            <img className="property-card__img" src={picture} alt="Indide of a property to rent"></img>
             <div className="property-card__overlay"></div>
             <h2 className="property-card__title">{title}</h2>
         </a>
